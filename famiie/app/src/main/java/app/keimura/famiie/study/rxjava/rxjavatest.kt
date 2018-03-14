caf66b695717fd9a5a6e7f93500e032f501d8b7e
keiMura <ke_stupidman01@yahoo.co.jp>
2018-03-15 08:28:33 +0900

    勉強用に一旦コミット2 - ナビゲーションメニューをとりあえず配置   - あとで整備する

--- famiie/app/src/main/java/app/keimura/famiie/study/rxjava/rxjavatest.kt
+++ famiie/app/src/main/java/app/keimura/famiie/study/rxjava/rxjavatest.kt
@@ -3,14 +3,19 @@ package app.keimura.famiie.study.rxjava
 import io.reactivex.BackpressureStrategy
 import io.reactivex.Flowable
 import io.reactivex.schedulers.Schedulers
+import org.reactivestreams.Subscriber
 import org.reactivestreams.Subscription
 
 /**
  * RxJavaの勉強用クラス。
  */
 class rxjavatest {
-    private lateinit var subscription: Subscription
+    // subscribeに指定するSubscriberのところでラムダだとフィールド（プロパティ）が持てないので保持。
+    lateinit var subscription: Subscription
 
+    /**
+     * 文字列の配列のデータを順番に実行し、最後にonCompleteメソッドが呼ばれるサンプル。
+     */
     fun main() {
         val flowable: Flowable<String> = Flowable.create<String>({ emitter ->
             val datas: Array<String> = arrayOf("Hello, World!", "こんにちは世界")
@@ -31,7 +36,7 @@ class rxjavatest {
                     // 実行しているスレッド名の取得
                     val threadName = Thread.currentThread().name;
                     println("$threadName : $data")
-                    this.subscription.request(1L)
+                    subscription.request(1L)
                 }, { error: Throwable ->
                     // エラーを通知された際の処理
                     // onError()
@@ -41,7 +46,7 @@ class rxjavatest {
                     // onComplete
                     val threadName = Thread.currentThread().name;
                     println("$threadName : 完了しました。")
-                }, { subscription: org.reactivestreams.Subscription ->
+                }, { subscription: Subscription ->
                     // 購読開始時の処理
                     // onSubscribe
                     this.subscription = subscription
@@ -50,4 +55,59 @@ class rxjavatest {
 
         Thread.sleep(500L)
     }
+
+    /**
+     * main()のSubscriberをラムダじゃない方法にしてSubscriptionを保持するように。
+     */
+    fun main2() {
+        val flowable: Flowable<String> = Flowable.create<String>({ emitter ->
+            val datas: Array<String> = arrayOf("Hello, World!", "こんにちは世界")
+            datas.forEach {
+                if (emitter.isCancelled) {
+                    return@create
+                }
+                emitter.onNext(it)
+            }
+            emitter.onComplete()
+        }, BackpressureStrategy.BUFFER) // 超過した場合はバッファに。
+
+        flowable
+                .observeOn(Schedulers.io()) // これを指定すると次のObservableがメインスレッドなのかバックグラウンドスレッドなのか決められる
+                .subscribe(object : Subscriber<String> {
+                    lateinit var subscription: Subscription
+
+                    override fun onSubscribe(subscription: Subscription) {
+                        this.subscription = subscription
+                        this.subscription.request(1L)
+                    }
+
+                    override fun onNext(data: String?) {
+                        val threadName = Thread.currentThread().name
+                        println("$threadName : $data")
+                        this.subscription.request(1L)
+                    }
+
+                    override fun onError(error: Throwable) {
+                        error.printStackTrace()
+                    }
+
+                    override fun onComplete() {
+                        val threadName = Thread.currentThread().name
+                        println("$threadName : 完了しました。")
+                    }
+                })
+
+        Thread.sleep(500L)
+    }
+
+    /********************************************************************************************************************************/
+/* BuckpressureStrategy |説明                                                                                                    */
+/* ----------------------------------------------------------------------------------------------------------------------------- */
+/* BUFFER               | 通知されるまで、すべてのデータをバッファする                                                                 */
+/* DROP                 | データを通知できるようになるまで、新たに生成されたデータを破棄する                                              */
+/* LATEST               | 生成した最新のデータのみをバッファし、生成されるたびにバッフするデータを置き換える                                */
+/* ERROR                | 通知街のデータがバッファ・サイズを超す場合はMissingBackpressureExceptionのエラーを通知する                     */
+/* NONE                 | 特定の処理を行わない。主にonBackpressureで始まるメソッドを使ってバックプレッシャーのモードを設定する場合に使われる。 */
+    /*********************************************************************************************************************************/
+
 }
